c1edc7b0d0a3fe742c22be9e321f0a1042c38d26
Barira <binteayoub139@gmail.com>
2019-10-08 08:39:52 +0500

    my first commit

---
+++ assignment5.py
@@ -0,0 +1,9 @@
+x=6
+print(x)
+y=x+7
+print(y)
+
+print('hello world')
+print('you must be the legendry got that comes from the sky')
+print('we have been waiting for you for a long time')
+print('our legends says you will be very tasty with mustard')
